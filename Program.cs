6ed32fe57dd89d50a12cb4e5dd769cf2411c6b05
unknown <nellysherova@mail.ru>
2021-01-19 17:42:39 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("x = "); int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write(x);
+        }
+    }
+}
